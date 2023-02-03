51b78394af93f0345ecf67b25d3e910d399279e7
kortor19 <benardkortor@gmail.com>
2023-02-03 12:38:04 -0800

    updates printf

--- functions1.c
+++ functions1.c
@@ -19,7 +19,7 @@ int print_unsigned(va_list types, char buffer[],
 	int i = BUFF_SIZE - 2;
 	unsigned long int num = va_arg(types, unsigned long int);
 
-	num = convert_size_unsgned(num, size);
+	num = convert_size_unsgnd(num, size);
 
 	if (num == 0)
 		buffer[i--] = '0';
